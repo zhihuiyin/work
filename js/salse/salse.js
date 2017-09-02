c2ec226f1a0f5ad2cba497cdf05dcc86f7c61ef8
18251953430@163.com <18251953430@163.com>
2017-09-02 15:52:07 +0800

    debug

--- js/salse/salse.js
+++ js/salse/salse.js
@@ -9,6 +9,11 @@ define([
 
     return function() {
         $('#container').html(salseTpl);
+
+        var gallery = mui('.mui-slider');
+        gallery.slider({
+            interval: 2000 //自动轮播周期，若为0则不自动播放，默认为0；
+        });
     }
 
 });
