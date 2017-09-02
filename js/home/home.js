5e2105b97a26682c04cbe10260c4eb52179c0811
18251953430@163.com <18251953430@163.com>
2017-09-02 16:17:23 +0800

    debug

--- js/home/home.js
+++ js/home/home.js
@@ -9,7 +9,10 @@ define([
     return function() {
         $('#container').html(homeTpl);
 
-
+        var gallery = mui('.mui-slider');
+        gallery.slider({
+            interval: 2000 //自动轮播周期，若为0则不自动播放，默认为0；
+        });
     }
 
 });
