5fe1ab7ea24a893b1ba92edf404a25b3fad62c59
18251953430@163.com <18251953430@163.com>
2017-09-11 09:58:38 +0800

    debug

--- js/main.js
+++ js/main.js
@@ -95,4 +95,17 @@ require(['zepto', 'mui', 'home/home', 'sort/sort', 'salse/salse', 'shopcar/shopc
             $('#modal-box').css('display', 'none');
         })
     })
+
+    // 阻止a标签的默认事件
+    $('body').find('a').click(function(e) {
+        //如果提供了事件对象，则这是一个非IE浏览器 
+        if (e && e.preventDefault) {
+            //阻止默认浏览器动作(W3C) 
+            e.preventDefault();
+        } else {
+            //IE中阻止函数器默认动作的方式 
+            window.event.returnValue = false;
+            return false;
+        }
+    });
 });
